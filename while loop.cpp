8e9616bac4bd90e1064f170dc0781a206f9c15a1
walter-kimutai <114175891+walter-kimutai@users.noreply.github.com>
2022-10-05 15:38:09 +0300

    Add files via upload

---
+++ while loop.cpp
@@ -0,0 +1,12 @@
+// C++ Program to print numbers from 2 to 25
+#include <iostream>
+using namespace std;
+int main() {
+ int i = 2;
+ while (i <= 25) {
+ cout << i << " ";
+ ++i;
+ }
+
+ return 0;
+}
